30cb294bb83ab7b092b8b5701c6cd32ee013baa1
Dominik Brame <idominikbrame@gmail.com>
2021-05-04 12:52:12 -0500

    completed assignemt, all tests passed

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,31 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if (num % 2 == 0) {
+    return true
+  } 
+  return false
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  let total = 0;
+  arr.forEach(element => {
+    total += element
+  });
+  return total
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  for(let i = 0; i < arr.length; i++) {
+    let element = arr[i];
+    for(let j = element + 1; j < arr.length; j++) {
+      if(arr[j] + element == sum) {
+        return true
+      }
+    }
+  } 
+  return false
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,22 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str = str.split(delim)
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let array = []
+  str = str.split('')
+  for(let i = 0; i < str.length; i+2) {
+  array.push(str.splice(0,2).join(''))
+  }
+  return array
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let strReverse = str.split('').reverse().join().replace(/,/g, "")
+  return strReverse
 }
 
 module.exports = {
